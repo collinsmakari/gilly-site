bbd52feee4b051e0fbffe73feef35b1b8953bd5a
AMORMAKARI <makaricollins287@gmail.com>
2026-01-18 10:11:38 +0300

    screen responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      {/* App container */}
+      <div className="flex min-h-screen flex-col">
+        {/* Fixed / Sticky Navbar */}
+        <Navbar />
+
+        {/* Main content area */}
+        <main className="flex-1 w-full overflow-x-hidden">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </Router>
   );
 }
